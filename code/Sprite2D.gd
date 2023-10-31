461d6822a1363f74d60d303ef42a4dc79fd6548a
Carlos Nunez <80909466+carlosnunez09@users.noreply.github.com>
2023-10-30 20:54:10 -0400

    cupple changes

--- code/Sprite2D.gd
+++ code/Sprite2D.gd
@@ -1,10 +1,13 @@
-extends Sprite2D
+extends RigidBody2D 
 
 
 var projectile_scene = preload("res://prefab/shootable.tscn")
 var shoot_velocity = 1000
-
 var speed = 100.0
+var fire_rate = 0.1
+
+var can_fire = true
+
 
 var object_position = Vector2(0, 0)
 var key_directions = {
@@ -19,7 +22,10 @@ var key_directions = {
 func _ready():
 	object_position = position
 
-func _process(delta):
+	# Create and set up the timer
+
+
+func _physics_process(delta):
 	var velocity = Vector2(0, 0)
 
 	for key in key_directions.keys():
@@ -34,23 +40,34 @@ func _process(delta):
 
 	position = object_position
 	
+	look_at(get_global_mouse_position())
 	
-	# Calculate the mouse position in every frame
-	var mouse_pos_xy = get_global_mouse_position()
-
-	# Calculate the direction vector from the sprite to the mouse
-	var target_direction = mouse_pos_xy - position
-
-	# Calculate the rotation angle
-	var rotation_angle = atan2(target_direction.y, target_direction.x)
-
-	# Apply the rotation
-	rotation = rotation_angle
+	#var mouse_pos_xy = get_global_mouse_position()
+	#var target_direction = mouse_pos_xy - position
+	#var rotation_angle = atan2(target_direction.y, target_direction.x)
+	#rotation = rotation_angle
 	
-	if Input.is_action_pressed("fire"):
+	
+	
+	
+	
+	
+	pass
+
+
+func _process(delta):
+	
+	
+	
+	
+	if Input.is_action_pressed("fire") and can_fire:
 		var bullet_ins = projectile_scene.instantiate()
 		bullet_ins.position = $bulletpoint.global_position
 		bullet_ins.rotation_degrees = rotation
 		bullet_ins.apply_impulse(Vector2(cos(rotation), sin(rotation)) * shoot_velocity, Vector2())
 		get_tree().get_root().add_child(bullet_ins)
+		can_fire = false
+		await get_tree().create_timer(fire_rate).timeout
+		can_fire = true
 		print("fire")
+
